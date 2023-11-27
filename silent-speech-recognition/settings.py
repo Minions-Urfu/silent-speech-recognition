7808c1dadd6e69cb1b110823229fda69eb6f0b23
lenurseytasanov <lenur.seytasanov@mail.ru>
2023-11-27 19:57:04 +0500

    congigure static files settings

--- silent-speech-recognition/settings.py
+++ silent-speech-recognition/settings.py
@@ -117,7 +117,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = '/home/LenurSeytasanov/silent-speech-recognition/silent-speech-recognition/static'
+STATIC_ROOT = '/home/LenurSeytasanov/silent-speech-recognition/main/static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
